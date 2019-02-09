f80eec569a3ccc5c5c7be1660483af7004b577cf
Lefroel <46615502+Lefroel@users.noreply.github.com>
2019-02-09 10:37:36 +0700

    Update tetrad.py

--- tetrad.py
+++ tetrad.py
@@ -2,6 +2,9 @@ from arcade import *
 WIDTH = 1600
 HEIGHT = 800
 CELL = 20
+open_window(WIDTH, HEIGHT, "Main Menu")
+set_background_color([255, 255, 255])
+start_render()
 
 
 def draw_tetrad():
@@ -16,9 +19,20 @@ def draw_tetrad():
         draw_line(0, CELL * i, WIDTH, CELL * i, [21, 18, 237], 2)
 
 
-open_window(WIDTH, HEIGHT, "Main Menu")
-set_background_color([255, 255, 255])
-start_render()
+def tochkodetector_x(x):
+    temp = x*CELL
+    return temp
+
+
+def tochkodetector_y(y):
+    temp = y * CELL
+    return temp
+
+
 draw_tetrad()
+draw_text("5", 1400, 150, [255, 0, 0], 60)
+draw_text("Молодец!", 1250, 100, [255, 0, 0], 40)
+draw_text("Домашняя работа.", tochkodetector_x(10), tochkodetector_y(37), [0, 213, 0], 40)
+draw_circle_outline(tochkodetector_x(50), tochkodetector_y(25), 123, [0, 213, 0], 10)
 finish_render()
 run()
